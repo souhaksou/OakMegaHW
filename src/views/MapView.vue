e8c36c2f4c6bd6a293e8cfbb890c6f2c581e6d7f
souhaksou <98225683+souhaksou@users.noreply.github.com>
2025-11-18 01:56:00 +0800

    add filter

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,22 +4,18 @@
     <br>
   </div>
   <div class="max-w:800 mx:auto flex jc:start ai:center">
-    <button @click="acceptLocation" class="inline-block p:4|8 r:4 b:1|solid|black">
-      <i class="f:14 bi bi-person-walking"></i><span class="ml:4">移動</span>
-    </button>
-    <div class="w:32"></div>
     <div>
       <span>距離</span>
       <select v-model="maxDistance" class="mx:8">
-        <option :value="1.0">1.0</option>
-        <option :value="1.5">1.5</option>
-        <option :value="2.0">2.0</option>
+        <option :value="1">1</option>
+        <option :value="2">2</option>
+        <option :value="3">3</option>
       </select>
       <span>km</span>
     </div>
     <div class="w:32"></div>
     <div>
-      <span>顯示</span>
+      <span>最多</span>
       <select v-model="maxCount" class="mx:8">
         <option :value="10">10</option>
         <option :value="15">15</option>
@@ -28,14 +24,21 @@
       <span>筆</span>
     </div>
     <div class="w:32"></div>
-    <div><input type="text" class="inline-block p:4|8 r:4 b:1|solid|black mr:8">
-      <button class="inline-block p:4|8 r:4 b:1|solid|black"><i class="bi bi-search"></i></button>
+    <button @click="searchData" class="inline-block p:4|8 r:4 b:1|solid|black">
+      <i class="f:14 bi bi-person-walking"></i><span class="ml:4">查詢</span>
+    </button>
+    <div class="w:32"></div>
+    <div><input ref="keywordInput" v-model="keyword" type="text" class="inline-block p:4|8 r:4 b:1|solid|black mr:8">
+      <button @click="clearKeyword" class="inline-block p:4|8 r:4 b:1|solid|black"><i class="bi bi-x"></i></button>
     </div>
   </div>
-  <div>
+  <div class="max-w:800 mx:auto">
     <p>list</p>
-    <div v-for="(item, index) in filterData" :key="item.id">
-      <p><span>第 {{ index }} 項</span>{{ item }}</p>
+    <div v-for="item in filteredList" :key="item.id" @click="twinklePin(item.id)"
+      class="flex jc:space-between ai:center">
+      <p>{{ item.stop_name }}</p>
+      <p>{{ item.name }}</p>
+      <p>{{ item.distance }} km</p>
     </div>
   </div>
 </template>
@@ -43,6 +46,7 @@
 <script setup>
 import { ref, shallowRef, computed, onMounted, onUnmounted } from 'vue';
 import L from 'leaflet';
+import goldIconImg from '@/assets/goldIcon.png';
 
 // 測試資料
 import api1 from '@/assets/api1.json';
@@ -124,11 +128,16 @@ const acceptLocation = () => {
   }
   console.log(currentPin.value);
   map.value.panTo([...currentPin.value]);
-  drawMarkers(JSON.parse(JSON.stringify(filterData.value)));
 };
 
 const list = ref([]);
 let markers = [];
+const goldIcon = L.icon({
+  iconUrl: goldIconImg,
+  iconSize: [27, 44],   // 輕微放大版
+  iconAnchor: [13, 44], // anchor 也要跟著調整
+});
+
 const drawMarkers = (list) => {
   // 先清除舊的 markers
   markers.forEach(marker => {
@@ -144,45 +153,65 @@ const drawMarkers = (list) => {
       ${item.name}<br />
       距離：${item.distance}
     `);
-    // 把新 marker 存入 markers 陣列
+    marker._originalIcon = marker.options.icon;
+    marker.itemId = item.id;
     markers.push(marker);
   });
 };
 
 const maxDistance = ref(1.0);
 const maxCount = ref(10);
-
-const filterData = computed(() => {
-  const temp = JSON.parse(JSON.stringify(list.value));
+const searchData = () => {
+  const rawData = api1.result;
+  list.value = filterData(rawData, maxDistance.value, maxCount.value);
+  acceptLocation();
+  drawMarkers(list.value);
+};
+const filterData = (data, maxDistance = 1, maxCount = 10) => {
   // 過濾距離
-  let nearbyList = temp
-    .filter(item => item.distance <= maxDistance.value)
-    .slice(0, maxCount.value);
+  let nearbyList = data
+    .filter(item => item.distance <= maxDistance)
+    .slice(0, maxCount);
   // 若指定距離內沒有任何資料 → fallback 前 N 筆
   if (nearbyList.length === 0) {
-    nearbyList = temp.slice(0, maxCount.value);
+    nearbyList = data.slice(0, maxCount);
   }
-  return nearbyList.map(({ id, stop_name, name, longitude, latitude, distance }) =>
-    ({ id, stop_name, name, longitude, latitude, distance })
-  );
-});
-// const filterData = (data, maxDistance = 1, maxCount = 10) => {
-//   // 過濾距離
-//   let nearbyList = data
-//     .filter(item => item.distance <= maxDistance)
-//     .slice(0, maxCount);
+  return nearbyList;
+};
 
-//   // 若指定距離內沒有任何資料 → fallback 前 N 筆
-//   if (nearbyList.length === 0) {
-//     nearbyList = data.slice(0, maxCount);
-//   }
-//   return nearbyList;
-// };
+const keyword = ref('');
+const keywordInput = ref(null);
+const clearKeyword = () => {
+  keyword.value = '';
+  keywordInput.value.focus();
+};
+const filteredList = computed(() => {
+  return list.value.filter(item => item.stop_name.toLowerCase().includes(keyword.value.toLowerCase()));
+});
+
+const twinklePin = (id) => {
+  const target = markers.find(m => m.itemId === id);
+  if (!target) return;
+
+  // 移動地圖到該 marker
+  map.value.setView(target.getLatLng());
+
+  // 換成你匯入的黃色 icon
+  target.setIcon(goldIcon);
+
+  // 1 秒後換回原本的（預設藍色那顆）
+  setTimeout(() => {
+    if (target._originalIcon) {
+      target.setIcon(target._originalIcon);
+    }
+  }, 1000);
+};
+
+
+
 
 onMounted(() => {
   initMap();
-  const rawData = api1.result;
-  list.value = rawData;
 });
 
 onUnmounted(() => {
